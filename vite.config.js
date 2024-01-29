d43efc6e3ff9619379e9c588e5a8ef79f83ad088
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-01-29 18:21:55 +0100

    build: use environment variable for base URI

--- vite.config.js
+++ vite.config.js
@@ -2,47 +2,40 @@ import pkg from './package.json';
 import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import vue from '@vitejs/plugin-vue';
 import { URL, fileURLToPath } from 'node:url';
-import { defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
-export default ({ mode }) =>  {
+export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-
-  base: '/indah/ui',
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => [
-            'detail-etab',
-            'page-param-etab',
-            'list-etab',
-            'image-cropper',
-            'param-etab'
-          ].includes(tag),
+    base: process.env.VITE_BASE_URI,
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) =>
+              ['detail-etab', 'page-param-etab', 'list-etab', 'image-cropper', 'param-etab'].includes(tag),
+          },
         },
+      }),
+      vueI18n({}),
+      cssInjectedByJsPlugin(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }),
-    vueI18n({}),
-    cssInjectedByJsPlugin(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  build: {
-    lib: {
-      entry: './src/main.ts',
-      name: pkg.name
-    }
-  },
-  define: {
-    'process.env': process.env,
-  },
-})
-}
-
+    build: {
+      lib: {
+        entry: './src/main.ts',
+        name: pkg.name,
+      },
+    },
+    define: {
+      'process.env': process.env,
+    },
+  });
+};
