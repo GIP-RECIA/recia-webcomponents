ba30c4709bbd201de370cd235baff5d199e591e4
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-01-29 18:25:31 +0100

    build: migrate vite configuration to typescript

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
