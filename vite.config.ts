1aac7fe2961fd898a1185634a748ce74c2cedb5b
Indah Juliani <indah.juliani@recia.fr>
2024-03-01 10:47:18 +0100

    feat: remove css-injected-by-js

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import vue from '@vitejs/plugin-vue';
 import { URL, fileURLToPath } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -21,7 +20,6 @@ export default ({ mode }: { mode: string }) => {
         },
       }),
       vueI18n({}),
-      cssInjectedByJsPlugin(),
     ],
     resolve: {
       alias: {
