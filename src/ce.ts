a17384d4d55f24e436acb3e4216f9c2795bb6aae
Manon Cornu <manon.cornu@recia.fr>
2024-04-23 10:45:57 +0200

    ci: set custom elements configuration

--- src/ce.ts
+++ src/ce.ts
@@ -1,10 +1,33 @@
+import CarteRessourceSFC from './components/CarteRessourceMediacentre.ce.vue';
+import I18nHostSFC from './components/I18nHost.ce.vue';
+import ListeRessourcesSFC from './components/ListeRessourcesMediacentre.ce.vue';
+import MenuMediacentreSFC from './components/MenuMediacentre.ce.vue';
+import PageMediacentreSFC from './components/PageMediacentre.ce.vue';
 import { defineCustomElement } from 'vue';
 
+const CarteRessource = defineCustomElement(CarteRessourceSFC);
+const I18nHost = defineCustomElement(I18nHostSFC);
+const ListeRessources = defineCustomElement(ListeRessourcesSFC);
+const Menu = defineCustomElement(MenuMediacentreSFC);
+const Page = defineCustomElement(PageMediacentreSFC);
+
 declare module 'vue' {
-  export interface GlobalComponents {}
+  export interface GlobalComponents {
+    CarteRessource: typeof CarteRessource;
+    I18nHost: typeof I18nHost;
+    ListeRessources: typeof ListeRessources;
+    Menu: typeof Menu;
+    Page: typeof Page;
+  }
 }
 
 // https://fr.vuejs.org/guide/extras/web-components#tips-for-a-vue-custom-elements-library
-const register = () => {};
+const register = () => {
+  customElements.define('carte-ressource', CarteRessource);
+  customElements.define('mediacentre-ui', I18nHost);
+  customElements.define('liste-ressources', ListeRessources);
+  customElements.define('menu', Menu);
+  customElements.define('page', Page);
+};
 
-export { register };
+export { CarteRessource, I18nHost, ListeRessources, Menu, Page, register };
