a894dadff62e1975902274ce4c2bed2fd9670c40
Manon Cornu <manon.cornu@recia.fr>
2024-04-25 09:53:57 +0200

    feat: add getFilters() for retrieve the user's list of filters

--- src/services/ServiceMediacentre.ts
+++ src/services/ServiceMediacentre.ts
@@ -1,15 +1,29 @@
 import { instance } from '@/utils/axiosUtils.ts';
 
-const getResources = async (baseApiUrl: string, userInfoApiUrl: string) => {
-  await instance.get(baseApiUrl);
+const getResources = async (baseApiUrl: string) => {
+  return await instance.get(baseApiUrl);
 };
 
-const addFavorite = async (baseApiUrl: string, userInfoApiUrl: string, idRessource: string) => {
-  await instance.post(`${baseApiUrl}/favorite/${idRessource}`, null);
+const addFavorite = async (baseApiUrl: string, idRessource: string) => {
+  const response = await instance.post(`${baseApiUrl}/favorite/${idRessource}`, null);
+  if (response.status === 200) {
+    return true;
+  } else {
+    throw new Error("Erreur lors de l'ajout de la ressource à vos favoris");
+  }
 };
 
-const removeFavorite = async (baseApiUrl: string, userInfoApiUrl: string, idRessource: string) => {
-  await instance.delete(`${baseApiUrl}/favorite/${idRessource}`);
+const removeFavorite = async (baseApiUrl: string, idRessource: string) => {
+  const response = await instance.delete(`${baseApiUrl}/favorite/${idRessource}`);
+  if (response.status === 200) {
+    return true;
+  } else {
+    throw new Error("Erreur lors de la suppression de la ressource de vos favoris, la ressource n'a pas été trouvée");
+  }
 };
 
-export { getResources, addFavorite, removeFavorite };
+const getFilters = async (baseApiUrl: string) => {
+  return await instance.get(`${baseApiUrl}/filtres`);
+};
+
+export { getResources, addFavorite, removeFavorite, getFilters };
