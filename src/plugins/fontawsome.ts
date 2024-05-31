0b4f0571662e873ba64a552b9f2ceacd91899266
Manon Cornu <manoncornu45@icloud.com>
2024-05-31 11:52:36 +0200

    chore: add FontAwesome icons

--- src/plugins/fontawsome.ts
+++ src/plugins/fontawsome.ts
@@ -1,9 +1,16 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
-import { faStar, faXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowUpRightFromSquare,
+  faCaretRight,
+  faCircleInfo,
+  faCircleNotch,
+  faStar,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
 
 const register = () => {
-  library.add(faXmark, faStar, farStar);
+  library.add(faXmark, faStar, farStar, faArrowUpRightFromSquare, faCircleNotch, faCircleInfo, faCaretRight);
 };
 
 export { register };
