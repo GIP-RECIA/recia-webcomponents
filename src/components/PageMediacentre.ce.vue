7b1b2b77d70466ca60c78efa1168b54c235ef110
Manon Cornu <manoncornu45@icloud.com>
2024-05-31 12:15:41 +0200

    feat: add filtering functions and refactoring

--- src/components/PageMediacentre.ce.vue
+++ src/components/PageMediacentre.ce.vue
@@ -1,69 +1,185 @@
 <script setup lang="ts">
-import { getFilters, getResources } from '../services/ServiceMediacentre.ts';
-import type { Filtre } from '@/types/FiltreType.ts';
+import { getFavorites, getFilters, getResources } from '../services/ServiceMediacentre.ts';
+import { setUserInfoApiUrl } from '../utils/soffitUtils.ts';
+import { getFilters as filtrage } from '@/services/ServiceFiltreMediacentre.ts';
+import type { Filtres } from '@/types/FiltresType.ts';
 // import { addFavorite, removeFavorite } from '../services/ServiceMediacentreTest.ts';
 import type { Ressource } from '@/types/RessourceType.ts';
-import { setUserInfoApiUrl } from '@/utils/soffitUtils.ts';
-import { onMounted, ref } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { onMounted, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
-const filtre = ref<string>('');
+const filtre = ref('tout');
 const ressources = ref<Array<Ressource>>([]);
-const filtres = ref<Array<Filtre>>([]);
+const filtres = ref<Array<Filtres>>([]);
 const chargement = ref<boolean>(false);
-
+const chargementApp = ref<boolean>(false);
+const filteredResources = ref<Array<Ressource>>([]);
 const props = defineProps<{
   baseApiUrl: string;
   userInfoApiUrl: string;
 }>();
 
-onMounted((): void => {
+onMounted(async (): Promise<void> => {
   setUserInfoApiUrl(props.userInfoApiUrl);
-  getRessources();
-  getFiltres();
+  await getRessources();
+  await getFiltres();
+  getResourcesByFilter(filtre.value, '');
 });
 
 const getRessources = async (): Promise<void> => {
   chargement.value = true;
-  let reponse = await getResources(props.baseApiUrl);
-  ressources.value = reponse.data;
+  try {
+    let reponse = await getResources(props.baseApiUrl);
+    ressources.value = reponse.data;
+  } catch (error: any) {
+    console.log(error);
+  } finally {
+    chargement.value = false;
+    if (chargementApp.value == false) {
+      chargementApp.value = true;
+    }
+  }
+};
+
+const updateFiltre = (value: CustomEvent): void => {
+  filtre.value = value.detail[0];
+  let idCategorie = value.detail[1];
+  if (filtre.value == 'favoris') {
+    getFavoris();
+  } else {
+    getResourcesByFilter(filtre.value, idCategorie);
+  }
+};
+
+const getFavoris = async (): Promise<void> => {
+  chargement.value = true;
+  try {
+    let reponse = await getFavorites(props.baseApiUrl);
+    // ressources.value = reponse.data;
+    console.log('ressources favorites : ', ressources.value);
+  } catch (error: any) {
+    console.log(error);
+  } finally {
+    chargement.value = false;
+  }
+};
+
+const getResourcesByFilter = (filtre: string, idCategorie: string): void => {
+  chargement.value = true;
+  try {
+    switch (idCategorie) {
+      case 'NIVEAU_EDUCATIF_FILTER':
+        filteredResources.value = ressources.value.filter((ressource) =>
+          ressource.niveauEducatif.some((e) => e.nom === filtre),
+        );
+        break;
+      case 'TYPE_PRESENTATION_FILTER':
+        filteredResources.value = ressources.value.filter((ressource) => ressource.typePresentation.code === filtre);
+        break;
+      case 'UAI_FILTER':
+        filteredResources.value = ressources.value.filter((ressource) =>
+          ressource.idEtablissement.some((e) => e.UAI === filtre),
+        );
+        break;
+      case 'DOMAINE_ENSEIGNEMENT_FILTER':
+        filteredResources.value = ressources.value.filter((ressource) =>
+          ressource.domaineEnseignement.some((e) => e.nom === filtre),
+        );
+        break;
+      default:
+        filteredResources.value = ressources.value;
+        break;
+    }
+  } catch (error: any) {
+    console.log(error);
+  }
   chargement.value = false;
 };
 
-const updateFiltre = (filtreSelect: string): void => {
-  filtre.value = filtreSelect;
-};
+// watch(filteredResources, (newValue) => {
+//   console.log(newValue);
+// });
 
 const getFiltres = async (): Promise<void> => {
   chargement.value = true;
-  let reponse = await getFilters(props.baseApiUrl);
-  filtres.value = reponse.data;
-  chargement.value = false;
+  try {
+    let reponse = await getFilters(props.baseApiUrl);
+    filtres.value = filtrage(ressources.value, reponse.data);
+  } catch (error: any) {
+    console.log(error);
+  } finally {
+    chargement.value = false;
+  }
 };
 </script>
 
 <template>
-  <div class="cadre-page-mediacentre">
-    <aside class="aside-page-mediacentre">
-      <menu-mediacentre :filtres="filtres" />
-    </aside>
+  <div v-if="!chargementApp" class="spinner-container">
+    <div class="spinner-element">
+      <font-awesome-icon icon="fa-solid fa-circle-notch" class="fa-spinner" />
+    </div>
+  </div>
+  <div v-else>
+    <div class="cadre-page-mediacentre">
+      <aside class="aside-page-mediacentre">
+        <menu-mediacentre :filtres="filtres" :checked="filtre" @update-checked="updateFiltre" />
+      </aside>
 
-    <main class="main-page-mediacentre">
-      <liste-ressources :filtre="filtre" :ressources="ressources" :chargement="chargement" />
-    </main>
+      <main class="main-page-mediacentre">
+        <liste-ressources
+          v-if="!chargement"
+          :filtre="filtre"
+          :ressources="filteredResources"
+          :chargement="chargement"
+          :baseApiUrl="baseApiUrl"
+          :userInfoApiUrl="userInfoApiUrl"
+        />
+      </main>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .cadre-page-mediacentre {
   display: flex;
   flex-direction: row;
+  gap: 3vh;
+  height: 100%;
+  width: 100%;
+}
+.spinner-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+.spinner-element {
+  color: #d3d3d3;
+  width: 10em;
+  height: 10em;
+}
+
+@keyframes spinner {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.fa-spinner {
+  animation: spinner 2s linear infinite;
 }
 
 .aside-page-mediacentre {
   max-width: min(25%, 290px);
-  width: 100%;
+  width: 100vh;
+  align-items: flex-start;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  padding: 3em 1em;
 }
 
 .main-page-mediacentre {
@@ -79,6 +195,10 @@ const getFiltres = async (): Promise<void> => {
   }
 
   .aside-page-mediacentre {
+    max-width: 100%;
+  }
+
+  .main-page-mediacentre {
     max-width: 100%;
   }
 }
