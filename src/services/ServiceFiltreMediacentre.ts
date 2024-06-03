7369ead00ffbcac68168b526e6639bcd0ae51954
Manon Cornu <manoncornu45@icloud.com>
2024-06-03 14:37:07 +0200

    fix: doesn't display empty categories

--- src/services/ServiceFiltreMediacentre.ts
+++ src/services/ServiceFiltreMediacentre.ts
@@ -5,43 +5,34 @@ import type { Ressource } from '@/types/RessourceType';
 
 const getFilters = (resources: Array<Ressource>, filterCategories: Array<string>): Array<Filtres> => {
   const filters: Array<Filtres> = [];
-  console.log(filterCategories);
   for (const filtre of filterCategories) {
-    filters.push(getFilterCategories(filtre, resources));
+    const values: Filtres | undefined = getFilterCategories(filtre, resources);
+    if (values != undefined) {
+      filters.push(values);
+    }
   }
-  console.log('avant de donner les filtres a pageMediacentre');
   return filters;
 };
 
-const getFilterCategories = (filter: string, resources: Array<Ressource>): Filtres => {
+const getFilterCategories = (filter: string, resources: Array<Ressource>): Filtres | undefined => {
   let values;
-  console.log('avant passage dans switch(filter)');
 
   switch (filter) {
     case 'NIVEAU_EDUCATIF_FILTER':
-      console.log('dans case niv edu');
-      console.log('etat avant set : ', resources.map((resource) => resource.niveauEducatif).flat());
       values = new Set(resources.map((resource) => resource.niveauEducatif).flat());
       values = Array.from(values);
-      console.log(values);
       break;
     case 'TYPE_PRESENTATION_FILTER':
-      console.log('dans case type pres');
       values = new Set(resources.map((resource) => resource.typePresentation).flat());
       values = Array.from(values);
-      console.log(values);
       break;
     case 'DOMAINE_ENSEIGNEMENT_FILTER':
-      console.log('dans case domaine');
       values = new Set(resources.map((resource) => resource.domaineEnseignement).flat());
       values = Array.from(values);
-      console.log(values);
       break;
     case 'UAI_FILTER':
-      console.log('dans case uai');
       values = resources.map((resource) => resource.idEtablissement).flat();
       values = [...new Set(values)];
-      console.log(values);
       break;
     default:
   }
@@ -49,9 +40,12 @@ const getFilterCategories = (filter: string, resources: Array<Ressource>): Filtr
   return convertToFiltres(values, filter);
 };
 
-const convertToFiltres = (values: Array<any>, filter: string): Filtres => {
+const convertToFiltres = (values: Array<any>, filter: string): Filtres | undefined => {
   const filters: Array<Filtre> = [];
   let filtres;
+  if (values.length == 0) {
+    return;
+  }
   switch (filter) {
     case 'NIVEAU_EDUCATIF_FILTER':
       values.forEach((item) => {
