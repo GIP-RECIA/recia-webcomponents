c4b84b03c092ff2465d6484217fd80ec8b14515c
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-07-22 11:20:11 +0200

    feat(soffit): parse jwt with jwt-decode library

--- packages/wisemapping/src/utils/soffitUtils.ts
+++ packages/wisemapping/src/utils/soffitUtils.ts
@@ -1,4 +1,5 @@
 import oidc, { type JWT } from '@uportal/open-id-connect';
+import { jwtDecode } from 'jwt-decode';
 
 let userInfoApiUrl: string;
 
@@ -12,18 +13,6 @@ const getJwt = async (): Promise<{ encoded: string; decoded: JWT }> => {
   return { encoded, decoded };
 };
 
-const parseJwt = (token: string): JWT => {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split('')
-      .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-      .join(''),
-  );
-
-  return JSON.parse(jsonPayload);
-};
+const parseJwt = (token: string): JWT => jwtDecode(token);
 
 export { setUserInfoApiUrl, getJwt, parseJwt };
