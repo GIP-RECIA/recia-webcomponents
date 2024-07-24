4c2d03991d2223c0c135e1046ca97376cb31a348
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-07-24 12:05:06 +0200

    chore: prepare merge into recia web components

--- packages/param-etab/vite.config.ts
+++ packages/param-etab/vite.config.ts
@@ -26,11 +26,6 @@ export default ({ mode }: { mode: string }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-    server: {
-      hmr: {
-        path: 'ws',
-      },
-    },
     build: {
       lib: {
         entry: './src/main.ts',
