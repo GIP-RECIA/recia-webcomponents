24cfbb9133cf12e4976bc3e1cf7837ecb4b14f06
Ewann ROUX <ewann.roux@recia.fr>
2024-09-25 15:36:43 +0200

    fix(mediacentre): replace hardcoded target of getConfig request with a .env key

--- packages/mediacentre/src/components/PageMediacentre.ce.vue
+++ packages/mediacentre/src/components/PageMediacentre.ce.vue
@@ -29,6 +29,7 @@ import { getConfig, getFavorites, getFilters, getResources, putFavorites } from 
 
 const props = defineProps<{
   baseApiUrl: string
+  configApiUrl: string
   userInfoApiUrl: string
   userRightsApiUrl: string
   getUserFavoriteResourcesApiUrl: string
@@ -59,7 +60,7 @@ onMounted(async (): Promise<void> => {
   try {
     chargementApp.value = true
     await initToken(props.userInfoApiUrl)
-    await getConfig(props.baseApiUrl)
+    await getConfig(props.configApiUrl)
     // await flushMediacentreFavorites(props.putUserFavoriteResourcesApiUrl, props.fnameMediacentreUi);
     await getRessources()
     await setFavoris()
