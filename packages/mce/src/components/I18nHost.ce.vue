76ef742d575b451dee7650d1d8c28c876ca75804
Indah Juliani <indah.juliani@recia.fr>
2024-09-27 14:57:32 +0200

    build: initialize project mce

---
+++ packages/mce/src/components/I18nHost.ce.vue
@@ -0,0 +1,32 @@
+<!--
+ Copyright (C) 2023 GIP-RECIA, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<script setup lang="ts">
+import { provide } from 'vue'
+import i18n from '@/i18n'
+import { I18nInjectionKey } from 'vue-i18n'
+
+provide(I18nInjectionKey, i18n)
+
+defineProps<{
+  mceApi: string
+  userInfoApiUrl: string
+}>()
+</script>
+
+<template>
+  <page-mce :mce-api="mceApi" :user-info-api-url="userInfoApiUrl"> </page-mce>
+</template>
