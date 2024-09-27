a983e5d803a99b3e666f109a3f156eb87992ff20
Indah Juliani <indah.juliani@recia.fr>
2024-09-27 14:57:32 +0200

    build: initialize project mce

---
+++ packages/mce/src/ce.ts
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2023 GIP-RECIA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { defineCustomElement } from 'vue'
+import I18nHostCe from './components/I18nHost.ce.vue'
+import PageMceCe from './components/PageMce.ce.vue'
+import OngletContentCe from './components/OngletContent.ce.vue'
+import ListOngletCe from './components/ListOnglet.ce.vue'
+import SectionOngletCe from './components/SectionOnglet.ce.vue'
+import InfoGeneralCe from './components/InfoGeneral.ce.vue'
+import RelationUserCe from './components/RelationUser.ce.vue'
+
+const I18nHost = defineCustomElement(I18nHostCe)
+const PageMce = defineCustomElement(PageMceCe)
+const OngletContent = defineCustomElement(OngletContentCe)
+const ListOnglet = defineCustomElement(ListOngletCe)
+const SectionOnglet = defineCustomElement(SectionOngletCe)
+const InfoGeneral = defineCustomElement(InfoGeneralCe)
+const RelationUser = defineCustomElement(RelationUserCe)
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    I18nHost: typeof I18nHost
+    PageMce: typeof PageMce
+    OngletContent: typeof OngletContent
+    ListOnglet: typeof ListOnglet
+    SectionOnglet: typeof SectionOnglet
+    InfoGeneral: typeof InfoGeneral
+    RelationUser: typeof RelationUser
+  }
+}
+
+const register = () => {
+  customElements.define('mce-ui', I18nHost)
+  customElements.define('page-mce', PageMce)
+  customElements.define('onglet-content', OngletContent)
+  customElements.define('list-onglet', ListOnglet)
+  customElements.define('section-onglet', SectionOnglet)
+  customElements.define('info-general', InfoGeneral)
+  customElements.define('relation-user', RelationUser)
+}
+
+export {
+  I18nHost,
+  PageMce,
+  OngletContent,
+  ListOnglet,
+  SectionOnglet,
+  InfoGeneral,
+  RelationUser,
+  register
+}
---
+++ packages/mce/src/types/relationType.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2023 GIP-RECIA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export type Relation = {
+  contact: number
+  eleve: Personne
+  uidRelation: string
+  displayNameRelation: string
+  autoriteParental: boolean
+  typeRelation: string
+  eleve2Contact: boolean
+  selected: boolean
+}
+
+export type Personne = {
+  uid: string
+}
