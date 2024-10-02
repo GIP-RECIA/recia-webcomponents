28cac440ad01c266f8d5cd82d9ee23b8ac983d50
Ewann ROUX <ewann.roux@recia.fr>
2024-10-02 10:06:43 +0200

    fix(mediacentre): move modal events definitions in PageMediacentre from Index.html

--- packages/mediacentre/src/components/PageMediacentre.ce.vue
+++ packages/mediacentre/src/components/PageMediacentre.ce.vue
@@ -53,6 +53,22 @@ const erreur = ref<string>('')
 
 const { t } = i18n.global
 
+let triggerElement: any;
+document.addEventListener('openModale', (event: any) => {
+  triggerElement = event.detail.originalEvent;
+  const modalElement: InfoModal = document.querySelector('info-modal');
+  modalElement.isOpen = !modalElement.isOpen;
+  modalElement.titleModal = event.detail.title;
+  modalElement.mainElement = document.querySelector('body > main, body > div');
+});
+
+document.addEventListener('closeModale', (event) => {
+  if (triggerElement) {
+    triggerElement.focus();
+    event.preventDefault();
+  }
+});
+
 const countNbFilteredResources = computed<number>(() => {
   return filteredResources.value.length
 })
