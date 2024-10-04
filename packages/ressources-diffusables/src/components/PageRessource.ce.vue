a2e8d5d701a0393028e233db8aae88a8310efe75
Ewann ROUX <ewann.roux@recia.fr>
2024-10-04 14:31:24 +0200

    feat(ressources-diffusables): add precise search by resource and editor name

--- packages/ressources-diffusables/src/components/PageRessource.ce.vue
+++ packages/ressources-diffusables/src/components/PageRessource.ce.vue
@@ -15,10 +15,16 @@
 -->
 
 <script setup lang="ts">
-import { getRessourcesDiffusables, getSize } from '@/services/serviceRessourcesDiffusables';
+import {
+  getRessourcesDiffusables,
+  getRessourcesDiffusablesWithRechercheFilter,
+  getSize,
+  getSizeWithRechercheFilter,
+} from '@/services/serviceRessourcesDiffusables';
 import type { Ressource } from '@/types/ressourceType';
 import { onMounted, ref } from 'vue';
 import { initToken } from '@/utils/axiosUtils';
+import { RechercheFilter } from '@/utils/RechercheFilter';
 
 const props = defineProps<{
   baseApiUrl: string;
@@ -34,6 +40,7 @@ const pageSuivante = ref<number>(0);
 const lectureTerminee = ref<boolean>(false);
 const chargement = ref<boolean>(false);
 const recherche = ref<string>('');
+const rechercheV2 = ref<RechercheFilter>();
 
 onMounted(async(): Promise<void> => {
   await initToken(props.userInfoApiUrl);
@@ -45,9 +52,19 @@ const reinitialiserRecherche = async (): Promise<void> => {
   recommencerRecherche();
 };
 
+const reinitialiserRechercheAvancee = async (rechercheInput: CustomEvent): Promise<void> => {
+  rechercheV2.value = rechercheInput.detail[0];
+  recommencerRechercheAvancee();
+};
+
 const recommencerRechercheInput = async (rechercheInput: CustomEvent): Promise<void> => {
   recherche.value = rechercheInput.detail[0];
   recommencerRecherche();
+};
+
+const recommencerRechercheAvanceeInput = async (rechercheInput: CustomEvent): Promise<void> => {
+  rechercheV2.value = rechercheInput.detail[0];
+  recommencerRechercheAvancee();
 };
 
 const recommencerRecherche = async (): Promise<void> => {
@@ -68,6 +85,31 @@ const recommencerRecherche = async (): Promise<void> => {
     } else {
       lectureTerminee.value = false;
       getPageSuivante();
+    }
+  } catch (e: any) {
+    erreur.value = e.toString() + (e.response != undefined ? ' | ' + e.response.data.message : '');
+    chargement.value = false;
+  }
+};
+
+const recommencerRechercheAvancee = async (): Promise<void> => {
+  ressources.value = [];
+  pageSuivante.value = 0;
+  erreur.value = '';
+  chargement.value = true;
+  try {
+    let response = await getSizeWithRechercheFilter(
+      props.baseApiUrl + props.ressourcesDiffusablesSizeApiUri,
+      props.userInfoApiUrl,
+      rechercheV2.value != undefined ? rechercheV2.value : new RechercheFilter(),
+    );
+    nombreRessourcesTotal.value = response.data.payload;
+    if (nombreRessourcesTotal.value === 0) {
+      lectureTerminee.value = true;
+      chargement.value = false;
+    } else {
+      lectureTerminee.value = false;
+      getPageSuivanteRechercheAvancee();
     }
   } catch (e: any) {
     erreur.value = e.toString() + (e.response != undefined ? ' | ' + e.response.data.message : '');
@@ -96,6 +138,28 @@ const getPageSuivante = async (): Promise<void> => {
     chargement.value = false;
   }
 };
+
+const getPageSuivanteRechercheAvancee = async (): Promise<void> => {
+  if (!lectureTerminee.value) {
+    erreur.value = '';
+    chargement.value = true;
+    try {
+      let response = await getRessourcesDiffusablesWithRechercheFilter(
+        props.baseApiUrl + props.ressourcesDiffusablesApiUri,
+        props.userInfoApiUrl,
+        pageSuivante.value++,
+        rechercheV2.value != undefined ? rechercheV2.value : new RechercheFilter(),
+      );
+      ressources.value = ressources.value.concat(response.data.payload);
+      if (ressources.value.length === nombreRessourcesTotal.value) {
+        lectureTerminee.value = true;
+      }
+    } catch (e: any) {
+      erreur.value = e.toString() + (e.response != undefined ? ' | ' + e.response.data.message : '');
+    }
+    chargement.value = false;
+  }
+};
 </script>
 
 <template>
@@ -107,6 +171,13 @@ const getPageSuivante = async (): Promise<void> => {
         @recommencer-recherche-input="recommencerRechercheInput"
         @reinitialiser-recherche="reinitialiserRecherche"
         ref="rechercheRessource"
+      />
+      <recherche-avancee-ressource
+        :nombre-ressources-total="nombreRessourcesTotal"
+        :nombre-ressources-affichees="ressources.length"
+        @recommencer-recherche-avancee-input="recommencerRechercheAvanceeInput"
+        @reinitialiser-recherche-avancee="reinitialiserRechercheAvancee"
+        ref="rechercheAvanceeRessource"
       />
       <legende-ressource class="legende-ressource-page-ressource" />
     </aside>
