d83cb92007be2d0d16cf10635575ac1035c95661
Ewann ROUX <ewann.roux@recia.fr>
2025-01-10 16:28:34 +0100

    feat(ressources-diffusables): create RessourcesPagination custom element (WIP)

--- packages/ressources-diffusables/src/plugins/fontawesome.ts
+++ packages/ressources-diffusables/src/plugins/fontawesome.ts
@@ -15,9 +15,23 @@
  */
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import { faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const register = () => {
   library.add(faXmark);
+  library.add(faEllipsisH);
+  library.add(faChevronLeft);
+  library.add(faChevronRight);
+  library.add(faAnglesRight);
+  library.add(faAnglesLeft);
+  library.add(faAngleLeft);
+  library.add(faAngleRight);
 };
 
 export { register };
