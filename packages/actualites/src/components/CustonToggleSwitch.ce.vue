83f8f3672fe1d2a38cec76e69de9bbc0d2318979
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-02-05 09:10:18 +0100

    refactor(actualites): reduce custom-toggle-switch

--- packages/actualites/src/components/CustonToggleSwitch.ce.vue
+++ packages/actualites/src/components/CustonToggleSwitch.ce.vue
@@ -18,114 +18,105 @@
 import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-// Définir les props
-const props = defineProps({
-  states: {
-    type: Array,
-    required: true,
-    default: () => ['all', 'read', 'unread'],
-  },
+const props = withDefaults(defineProps<{
+  states: Array<string>
+}>(), {
+  states: () => ['all', 'read', 'not-read'],
 })
 
-// Définir les événements émis
 const emit = defineEmits(['readStatus'])
 
-// État actuel
 const currentState = ref(props.states[0])
 
 const { t } = useI18n()
 
-// Fonction pour définir un nouvel état
 function setState(state: string) {
   if (props.states.includes(state)) {
     currentState.value = state
-    emit('readStatus', state === 'all' ? undefined : (state === 'read')) // Émet l'événement avec la nouvelle valeur
+    emit('readStatus', state === 'all' ? undefined : (state === 'read'))
   }
 }
 </script>
 
 <template>
-  <div class="toggle-switch">
-    <span
+  <ul>
+    <li
       v-for="(state, index) in props.states"
       :key="index"
-      :class="{ active: currentState === state }"
-      class="toggle-option"
-      tabindex="0"
-      @click="setState(state)"
-      @keydown.enter="setState(state)"
     >
-      <div v-if="state === 'all'" class="toggle-option-more">{{ t('switch.all') }}</div>
-      <div v-if="state === 'read'" class="toggle-option-more">{{ t('switch.read') }}</div>
-      <div v-if="state === 'unread'" class="toggle-option-more">{{ t('switch.not-read') }}</div>
-    </span>
-  </div>
+      <button :class="{ active: currentState === state }" @click="setState(state)">
+        {{ t(`switch.${state}`) }}
+      </button>
+    </li>
+  </ul>
 </template>
 
 <style lang="scss">
 @use '@/assets/global.scss' as *;
 
-.toggle-switch {
-  display: inline-flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #f0f0f0;
-  border-radius: 20px;
-  padding: 3px;
-  user-select: none;
+* {
+  box-sizing: border-box;
+}
+
+ul,
+li {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+button {
   border: none;
-  gap: 0.5rem;
+  background: none;
 }
 
-.toggle-option {
-  text-align: center;
-  color: #666;
-  transition:
-    color 0.3s,
-    background-color 0.3s;
-  padding: 5px 0;
-  border-radius: 20px;
-  font-family: $dm-sans;
-  font-size: 14px;
-  cursor: pointer;
-  width: auto;
-  justify-content: center;
-}
-
-.toggle-option:focus-visible {
-  background-color: $primary-transparent;
-  color: $primary;
-  outline: 3px solid $primary;
-}
-
-.toggle-option.active:focus-visible {
-  background-color: $primary;
-  color: $standard-colour-white;
-  outline: 3px solid $primary-transparent;
-}
-
-.toggle-option-more {
+ul {
   display: inline-flex;
-  padding-right: 1rem;
-  padding-left: 1rem;
-  width: auto;
-}
+  background-color: rgba($standard-colour-black, 0.05);
+  border-radius: 50px;
+  padding: 4px;
+  gap: 8px;
 
-/* Style pour l'état actif */
-.toggle-option.active {
-  background-color: #1e1e1e;
-  color: white;
-}
+  li > button {
+    text-align: center;
+    color: #666;
+    transition:
+      color 0.3s,
+      background-color 0.3s;
+    padding: 4px 12px;
+    border-radius: 50px;
+    font-family: $dm-sans;
+    font-size: 14px;
+    font-weight: 500;
+    cursor: pointer;
 
-/* Empêche les options actives d'avoir un effet au survol */
-.toggle-option.active:hover {
-  background-color: #1e1e1e;
-  color: $standard-colour-white;
-}
+    &.active {
+      background-color: $standard-colour-black;
+      color: white;
 
-/* Effet de survol pour les options non actives */
-.toggle-option:not(.active):hover {
-  background-color: rgba(0, 123, 255, 0.1); // Bleu transparent
-  color: $primary; // Texte bleu
+      &:hover {
+        background-color: $primary;
+      }
+
+      &:focus-visible {
+        background-color: $primary;
+        color: $standard-colour-white;
+        outline: 3px solid #0062bc4d;
+      }
+    }
+
+    &:not(.active) {
+      &:hover {
+        background-color: $primary-transparent;
+        color: $primary;
+      }
+
+      &:focus-visible {
+        background-color: $primary-transparent;
+        color: $primary;
+        outline: 3px solid $primary;
+      }
+    }
+  }
 }
 </style>
