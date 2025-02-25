eec177407a5fc5cf7811b0ca8d9915368cb0bdf9
indahj <indahjuliani87@gmail.com>
2025-02-25 10:32:29 +0100

    feat(mce): add avatar cropper modal

--- packages/mce/src/components/UserInfo.ce.vue
+++ packages/mce/src/components/UserInfo.ce.vue
@@ -28,54 +28,44 @@ defineProps<{
   identifiant: string
   mdp: boolean
   userPublic: string
+  userId: string
 }>()
 
 const { t } = useI18n()
 const m = (key: string): string => t(`user-info.${key}`)
-
-function modifAvatar() {
-  alert('change avatar')
-}
 </script>
 
 <template>
   <div class="profile-container">
     <div class="profile-picture">
-      <div class="image-container" @click="modifAvatar">
-        <img class="avatar" :src="avatar" alt="">
-
-        <button class="edit-picture">
-          <img class="edit-picture-icon" src="../assets/pen-solid.svg" alt="">
-        </button>
-      </div>
+      <avatar-user :avatar="avatar" :user="userId" />
       <b class="user-name">{{ userName }}</b>
     </div>
     <div class="profile-info">
       <div v-if="identifiant !== null">
-        <b>{{ m('login')}}</b>
+        <b>{{ m('login') }}</b>
         <div>{{ identifiant }}</div>
       </div>
       <div v-if="userMail !== null">
-        <b>{{ m('email')}}</b>
+        <b>{{ m('email') }}</b>
         <div>{{ userMail }}</div>
       </div>
       <div v-if="bod !== null">
-        <b>{{ m('bod')}}</b>
+        <b>{{ m('bod') }}</b>
         <div>{{ bod }}</div>
       </div>
       <div v-if="etab !== null">
-        <b>{{ m('structure')}}</b>
+        <b>{{ m('structure') }}</b>
         <div>{{ etab }}</div>
       </div>
       <div v-if="mdp === true">
-        <b>{{ m('password')}} <button>modifier</button></b>
+        <b>{{ m('password') }} <button>modifier</button></b>
         <div>*******</div>
       </div>
       <div v-if="userPublic?.length >= 1">
-        <b>{{ m('access')}}</b>
+        <b>{{ m('access') }}</b>
         <div>{{ userPublic }}</div>
       </div>
-
     </div>
   </div>
 </template>
@@ -94,57 +84,6 @@ function modifAvatar() {
     gap: 10px;
     align-items: center;
 
-    .image-container {
-      position: relative;
-      display: flex;
-      width: 130px;
-      height: 130px;
-      border-radius: 50%;
-      outline: solid #eef0f8;
-      box-shadow: 0 0.5px 2.5px 1px #00000080;
-
-      .avatar {
-        border-radius: 50%;
-        position: relative;
-        background-color: #26448a;
-      }
-
-      .edit-picture {
-        display: none;
-      }
-    }
-
-    .image-container:hover {
-      cursor: pointer;
-      outline: 3px solid #26448a;
-      box-shadow: 0 0.5px 2.5px 1px #00000080;
-
-      .avatar {
-        opacity: 0.4;
-      }
-
-      .edit-picture {
-        width: 35px;
-        height: 35px;
-        border: none;
-        border-radius: 50px;
-        bottom: 48px;
-        right: 48px;
-        cursor: pointer;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        outline: solid #eef0f8;
-        box-shadow: 0 0.5px 2.5px 1px #00000080;
-        position: absolute;
-
-        .edit-picture-icon {
-          width: 11px;
-          height: 11px;
-        }
-      }
-    }
-
     .user-name {
       text-align: center;
     }
@@ -160,25 +99,24 @@ function modifAvatar() {
     row-gap: 13.67px;
     flex-direction: column;
 
-      b {
-        color: black;
-        display: block;
-        font-size: 0.875em;
-        margin-top: 0.625em;
-        order: 1;
-        transition: all 0.25s;
-        font-weight: bold;
-      }
+    b {
+      color: black;
+      display: block;
+      font-size: 0.875em;
+      margin-top: 0.625em;
+      order: 1;
+      transition: all 0.25s;
+      font-weight: bold;
+    }
 
-      div {
-        order: 2;
-        outline: none;
-        top: 0.08rem;
-        border: 0;
-        border-bottom: 1px solid #eee;
-        font-weight: 300;
-      }
-
+    div {
+      order: 2;
+      outline: none;
+      top: 0.08rem;
+      border: 0;
+      border-bottom: 1px solid #eee;
+      font-weight: 300;
+    }
   }
 }
 
@@ -191,30 +129,6 @@ function modifAvatar() {
 
 @media (max-width: 815px) {
   .profile-container {
-    .profile-picture {
-      .image-container {
-        .edit-picture {
-          width: 35px;
-          height: 35px;
-          border: none;
-          border-radius: 50px;
-          left: 95px;
-          cursor: pointer;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          outline: solid #eef0f8;
-          box-shadow: 0 0.5px 2.5px 1px #00000080;
-          position: absolute;
-
-          .edit-picture-icon {
-            width: 11px;
-            height: 11px;
-          }
-        }
-      }
-    }
-
     .profile-info {
       background-color: white;
       padding: 15px;
