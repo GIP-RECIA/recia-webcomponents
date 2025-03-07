999bae6b89d16b65c4b1f64249f0f88f5c5911d8
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-03-07 15:59:22 +0100

    refactor(actualites): cleanup app dev

--- packages/actualites/src/AppDev.vue
+++ packages/actualites/src/AppDev.vue
@@ -26,44 +26,47 @@ const {
 </script>
 
 <template>
-  <div class="app-dev">
-    <carrousel-ui
-      :user-info-api-url="VITE_USER_INFO_API_URI"
-      :base-url="VITE_BASE_API_URL"
-      :get-user-news-url="VITE_USER_NEWS_URI"
-      :get-item-by-id-url="VITE_GET_ITEM_URI"
-      :get-news-reading-informations-url="VITE_GET_NEWS_READING_URI"
-      :set-reading-url="VITE_SET_NEWS_READING_URI"
-    />
-    <all-news
-      :user-info-api-url="VITE_USER_INFO_API_URI"
-      :base-url="VITE_BASE_API_URL"
-      :get-user-news-url="VITE_USER_NEWS_URI"
-      :get-item-by-id-url="VITE_GET_ITEM_URI"
-      :get-news-reading-informations-url="VITE_GET_NEWS_READING_URI"
-      :set-reading-url="VITE_SET_NEWS_READING_URI"
-    />
-  </div>
+  <carrousel-ui
+    :user-info-api-url="VITE_USER_INFO_API_URI"
+    :base-url="VITE_BASE_API_URL"
+    :get-user-news-url="VITE_USER_NEWS_URI"
+    :get-item-by-id-url="VITE_GET_ITEM_URI"
+    :get-news-reading-informations-url="VITE_GET_NEWS_READING_URI"
+    :set-reading-url="VITE_SET_NEWS_READING_URI"
+  />
+  <all-news
+    :user-info-api-url="VITE_USER_INFO_API_URI"
+    :base-url="VITE_BASE_API_URL"
+    :get-user-news-url="VITE_USER_NEWS_URI"
+    :get-item-by-id-url="VITE_GET_ITEM_URI"
+    :get-news-reading-informations-url="VITE_GET_NEWS_READING_URI"
+    :set-reading-url="VITE_SET_NEWS_READING_URI"
+  />
 </template>
 
 <style lang="scss">
-.app-dev {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 70%; // 80% de la largeur totale (10% de marge de chaque côté)
-  margin: 0 auto;
-  gap: 2em;
-}
+@use 'sass:map';
+@use '@/assets/global.scss' as *;
 
-all-news,
-carrousel-ui {
+#app {
   width: 100%;
+  padding-right: 16px;
+  padding-left: 16px;
+  margin-right: auto;
+  margin-left: auto;
+
+  @each $name, $value in $grid-breakpoints {
+    @if $value != 0 {
+      @media screen and (width >= $value) {
+        max-width: map.get($container-max-widths, $name);
+      }
+    }
+  }
 }
 
-@media (max-width: 768px) {
-  .app-dev {
-    width: 90%;
-  }
+carrousel-ui,
+all-news {
+  display: block;
+  margin: 2em 0;
 }
 </style>
