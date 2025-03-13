f6a29fe5a4c852ecc0a7de6483e0f57099010d2b
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-03-13 17:23:09 +0100

    refactor(actualites): i18n

--- packages/actualites/src/App.vue
+++ packages/actualites/src/App.vue
@@ -14,6 +14,61 @@
  limitations under the License.
 -->
 
+<script setup lang="ts">
+const {
+  VITE_USER_INFO_API_URI,
+  VITE_BASE_API_URL,
+  VITE_USER_NEWS_URI,
+  VITE_GET_ITEM_URI,
+  VITE_GET_NEWS_READING_URI,
+  VITE_SET_NEWS_READING_URI,
+} = import.meta.env
+</script>
+
 <template>
-  <div />
+  <carrousel-ui
+    :user-info-api-url="VITE_USER_INFO_API_URI"
+    :base-url="VITE_BASE_API_URL"
+    :get-user-news-url="VITE_USER_NEWS_URI"
+    :get-item-by-id-url="VITE_GET_ITEM_URI"
+    :get-news-reading-informations-url="VITE_GET_NEWS_READING_URI"
+    :set-reading-url="VITE_SET_NEWS_READING_URI"
+    all-news-page-url="#"
+  />
+  <all-news
+    :user-info-api-url="VITE_USER_INFO_API_URI"
+    :base-url="VITE_BASE_API_URL"
+    :get-user-news-url="VITE_USER_NEWS_URI"
+    :get-item-by-id-url="VITE_GET_ITEM_URI"
+    :get-news-reading-informations-url="VITE_GET_NEWS_READING_URI"
+    :set-reading-url="VITE_SET_NEWS_READING_URI"
+    back-url="#"
+  />
 </template>
+
+<style lang="scss">
+@use 'sass:map';
+@use '@/assets/global.scss' as *;
+
+#app {
+  width: 100%;
+  padding-right: 16px;
+  padding-left: 16px;
+  margin-right: auto;
+  margin-left: auto;
+
+  @each $name, $value in $grid-breakpoints {
+    @if $value != 0 {
+      @media screen and (width >= $value) {
+        max-width: map.get($container-max-widths, $name);
+      }
+    }
+  }
+}
+
+carrousel-ui,
+all-news {
+  display: block;
+  margin: 2em 0;
+}
+</style>
