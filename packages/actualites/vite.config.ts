ddbd1d21bb910e46ecd018ae31231259be328f7c
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-03-17 14:06:00 +0100

    build(actualites): refactor build system

--- packages/actualites/vite.config.ts
+++ packages/actualites/vite.config.ts
@@ -19,7 +19,7 @@ import { fileURLToPath } from 'node:url'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
-import pkg from './package.json'
+import { name } from './package.json'
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -62,7 +62,8 @@ export default ({ mode }: { mode: string }) => {
     build: {
       lib: {
         entry: './src/main.ts',
-        name: pkg.name,
+        formats: ['es'],
+        name,
       },
       sourcemap: true,
     },
