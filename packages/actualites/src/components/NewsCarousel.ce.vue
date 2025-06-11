971305c810242eea38828a7c659c53b2d33fb30b
Ewann ROUX <ewann.roux@recia.fr>
2025-06-11 11:48:39 +0200

    feat(actualites): add event listener to carousel for opening modal from js events

--- packages/actualites/src/components/NewsCarousel.ce.vue
+++ packages/actualites/src/components/NewsCarousel.ce.vue
@@ -16,10 +16,10 @@
 
 <script setup lang="ts">
 import type { PaginatedResult } from '@/types/PaginatedResult.ts'
+import { computed, onMounted, ref } from 'vue'
 import i18n from '@/plugins/i18n.ts'
 import { getNewsReadingInformations, getPaginatedNews } from '@/services/NewsService.ts'
 import { initToken, instance } from '@/utils/axiosUtils.ts'
-import { computed, onMounted, ref } from 'vue'
 
 const props = defineProps<{
   getItemByIdUrl: string
@@ -50,7 +50,21 @@ onMounted(async () => {
   finally {
     loading.value = false
   }
+  window.addEventListener(
+    'recia-actu-modal',
+    (e) => { handleModalEvent(e) },
+  )
 })
+
+function handleModalEvent(e: any) {
+  if (e instanceof CustomEvent) {
+    if (e.detail?.uuid) {
+      if (typeof (e.detail.uuid) === 'string' && e.detail.uuid.length > 0) {
+        openModal(e.detail.uuid)
+      }
+    }
+  }
+}
 
 const currentIndex = ref(0)
 
