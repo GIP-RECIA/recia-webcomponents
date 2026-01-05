b7a82131872691345abcc0c3f808df73f44cd873
Ewann ROUX <ewann.roux@recia.fr>
2026-01-05 15:43:26 +0100

    feat(sympa): add filter that put user choices in store

---
+++ packages/esup-sympa/src/components/FilterEsupSympa.ce.vue
@@ -0,0 +1,54 @@
+<!--
+ Copyright (C) 2023 GIP-RECIA, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { PermissionKey } from '@/types/permissionKeyEnum'
+import { checked } from '@/utils/store'
+import '@gip-recia/ui-webcomponents/dist/r-filters.js'
+
+const { t } = useI18n()
+const data = ref<Array<any>>([])
+
+onMounted(async (): Promise<void> => {
+  data.value = [
+    {
+      id: 'permissions',
+      name: 'Par permission:',
+      type: 'checkbox',
+      items: [
+        {
+          key: 'all',
+          value: 'Toutes les permissions',
+        },
+        ...Object.values(PermissionKey).map(x => ({ key: x, value: t(`permission-labels.${x}`) })),
+      ],
+    },
+  ]
+})
+
+async function updateChecked(event: CustomEvent) {
+  checked.value = event.detail.activeFilters[0].checked
+}
+</script>
+
+<template>
+  <r-filters
+    :data="data"
+    @update-filters="updateChecked"
+  />
+</template>
