dfd3db3bb2b5dc8ed086c41b4954b469ca5f86e6
ismail-durak <i.durak45000@gmail.com>
2026-06-11 11:54:49 +0200

    refactor:  implification et réorganisation bootstrap application Vue

--- packages/mce/src/main.ts
+++ packages/mce/src/main.ts
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
+import { createApp } from 'vue'
 import App from '@/App.vue'
 import { register as registerCustomElements } from '@/ce'
 import { register as registerFontAwsome } from '@/plugins/fontawesome'
 import i18n from '@/plugins/i18n'
-import { createApp } from 'vue'
 import 'regenerator-runtime/runtime.js'
 
 const app = createApp(App)
